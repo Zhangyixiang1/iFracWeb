b345cb9c7d47563aed97bb86e66d4adec9aba0cd
Zhangyixiang1 <31239955+Zhangyixiang1@users.noreply.github.com>
2018-12-10 17:49:56 +0800

    Add project files.

---
+++ iFracWeb/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iFracWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// d登陆方法验证
+        /// </summary>
+        /// <returns>OK 或者error</returns>
+        public string confirmation()
+        {
+            //获取前台传回的数据
+            string userid = Request["userid"];
+            string password = Request["password"];
+
+            //如果验证通过，则把数据存到session中，返回成功
+            if (userid == "admin" && password == "admin")
+            {
+                Session["userID"] = userid;
+
+                return "OK";
+            }
+            //验证不通过，返回失败
+            else
+            {
+
+                return "error";
+            }
+
+
+
+        }
+    }
+}
